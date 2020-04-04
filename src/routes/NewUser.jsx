10107bbbd14417d7629d46a4ced6743f07c5e947
Francisco Junior <jr-francis2011@hotmail.com>
2020-04-04 19:14:33 -0300

    Add New User

--- src/routes/NewUser.jsx
+++ src/routes/NewUser.jsx
@@ -2,7 +2,65 @@ import React from "react";
 import "./Feed.scss";
 
 class NewUser extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "",
+      avatar: "",
+      username: "",
+      email: "",
+      submit: false, 
+    }
+  }
+
+  setName(event) {
+    const { value } = event.target;
+
+    this.setState({ name: value });
+  }
+
+  setAvatar(event) {
+    const { value } = event.target;
+
+    this.setState({ avatar: value });
+  }
+
+  setUsername(event) {
+    const { value } = event.target;
+
+    this.setState({ username: value });
+  }
+
+  setEmail(event) {
+    const { value } = event.target;
+
+    this.setState({ email: value });
+  }
+
+  cadastraUsuario(event) {
+    event.preventDefault();
+
+    const { name, username, email, avatar} = this.state;
+    const postObject = JSON.stringify({
+      name,
+      avatar,
+      username,
+      email,
+    })
+
+    fetch("https://5e7d0266a917d70016684219.mockapi.io/api/v1/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: postObject,
+    }).then(() => this.setState({ submit: true })); 
+  }
+
   render() {
+    const { name, username, avatar, submit } = this.state;
+
     return (
       <div className="container">
         <section className="feed">
@@ -11,40 +69,50 @@ class NewUser extends React.Component {
               <h1>Novo usuário:</h1>
             </header>
 
+            {submit && window.alert('Cadastrado com sucesso')}
+
             <div className="user">
               <div className="user__thumb">
-                <img src="http://placehold.it/200x200" alt="" />
+              { avatar.length > 0
+                ? <img src={avatar} alt="" />
+                : <img src="http://placehold.it/200x200" alt="" />
+              }         
               </div>
-
-              <p className="user__name">Codenation</p>
+              <p className="user__name">{name} - @{username}</p>
             </div>
 
             <div className="post__form">
               <label>Nome</label>
               <input 
                 type="text" 
-                placeholder="Ex: Fulano da Silva" 
+                placeholder="Ex: Fulano da Silva"
+                onChange={(event) => this.setName(event)}
               />
 
               <label>Usuário</label>
               <input 
                 type="text" 
-                placeholder="Ex: fulano_da_silva" 
+                placeholder="Ex: fulano_da_silva"
+                onChange={(event) => this.setUsername(event)}
               />
 
               <label>E-mail</label>
               <input 
                 type="email" 
-                placeholder="Ex: fulano@provedor.com" 
+                placeholder="Ex: fulano@provedor.com"
+                onChange={(event) => this.setEmail(event)}
               />
 
               <label>Url da Imagem de Perfil</label>
               <input 
                 type="text" 
-                placeholder="http:// ..." 
+                placeholder="http:// ..."
+                onChange={(event) => this.setAvatar(event)} 
               />
 
-              <button type="button">
+              <button type="button"
+              onClick={(event) => this.cadastraUsuario(event)}
+              >
                 Cadastrar
               </button>
 
